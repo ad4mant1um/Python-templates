7bdede28be55f4e436507b6fe953540dd1b913f8
ad4mant1um <40131409+ad4mant1um@users.noreply.github.com>
2018-06-18 16:32:04 +0200

    Fix button.py

--- button.py
+++ button.py
@@ -2,24 +2,24 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-class ButtonWindow(gtk.Window):
+class ButtonWindow(Gtk.Window):
 
     def __init__(self):
-        gtk.Window.__init__(self, title="Button")
+        Gtk.Window.__init__(self, title="Buttons")
         self.set_border_width(10)
 
-        hbox = gtk.Box(spacing=7)
+        hbox = Gtk.Box(spacing=7)
         self.add(hbox)
 
-        button = gtk.Button.new_with_label("Test")
+        button = Gtk.Button.new_with_label("Test")
         button.connect("clicked", self.on_click_me_clicked)
         hbox.pack_start(button, True, True, 0)
 
-        button = gtk.Button.new_with_mnemonic("_Open")
+        button = Gtk.Button.new_with_mnemonic("_Open")
         button.connect("clicked", self.on_open_clicked)
         hbox.pack_start(button, True, True, 0)
 
-        button = gtk.Button.new_with_mnemonic("_Close")
+        button = Gtk.Button.new_with_mnemonic("_Close")
         button.connect("clicked", self.on_close_clicked)
         hbox.pack_start(button, True, True, 0)
 
@@ -31,9 +31,9 @@ class ButtonWindow(gtk.Window):
 
     def on_close_clicked(self, button):
         print("Closing application")
-        gtk.main_quit()
+        Gtk.main_quit()
 
 win = ButtonWindow()
-win.connect("destroy", gtk.main_quit)
+win.connect("destroy", Gtk.main_quit)
 win.show_all()
-gtk.main()
+Gtk.main()
